5dc4d0962d29b70f007d8ee9be49fb800326a9bd
aminsholeh <amin11lfc@gmail.com>
2018-11-29 02:33:32 +0700

    first commit 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        
+        <br/>
+        <br/>
+        <h3> Footer</h3>
+      </div>
+    )
+  }
+}
---
+++ src/pages/Vote.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Vote extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Vote</h1>
+      </div>
+    )
+  }
+}
